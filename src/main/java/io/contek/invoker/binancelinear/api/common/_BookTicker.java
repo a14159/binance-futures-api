21f8e3b364d1de500a2f22042faa4ad0bc6e7657
kong <kong_k@hotmail.com>
2022-11-25 17:28:42 +0200

    DTO fixes

--- src/main/java/io/contek/invoker/binancelinear/api/common/_BookTicker.java
+++ src/main/java/io/contek/invoker/binancelinear/api/common/_BookTicker.java
@@ -1,14 +1,15 @@
 package io.contek.invoker.binancelinear.api.common;
 
 import javax.annotation.concurrent.NotThreadSafe;
+import java.math.BigDecimal;
 
 @NotThreadSafe
 public class _BookTicker {
 
   public String symbol;
-  public Double bidPrice;
-  public Double bidQty;
-  public Double askPrice;
-  public Double askQty;
-  public Long time;
+  public BigDecimal bidPrice;
+  public BigDecimal bidQty;
+  public BigDecimal askPrice;
+  public BigDecimal askQty;
+  public long time;
 }
--- src/main/java/io/contek/invoker/binancelinear/api/common/_UserTrade.java
+++ src/main/java/io/contek/invoker/binancelinear/api/common/_UserTrade.java
@@ -1,22 +1,23 @@
 package io.contek.invoker.binancelinear.api.common;
 
 import javax.annotation.concurrent.NotThreadSafe;
+import java.math.BigDecimal;
 
 @NotThreadSafe
 public class _UserTrade {
 
-  public Boolean buyer;
-  public String commission;
+  public boolean buyer;
+  public BigDecimal commission;
   public String commissionAsset;
-  public Long id;
-  public Boolean maker;
-  public Long orderId;
-  public String price;
-  public String qty;
-  public String quoteQty;
-  public String realizedPnl;
+  public long id;
+  public boolean maker;
+  public long orderId;
+  public BigDecimal price;
+  public BigDecimal qty;
+  public BigDecimal quoteQty;
+  public BigDecimal realizedPnl;
   public String side;
   public String positionSide;
   public String symbol;
-  public Long time;
+  public long time;
 }
